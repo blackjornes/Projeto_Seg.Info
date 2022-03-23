ac1d76266f34edad8f7ef090b73a37b23bd12001
Bjorn Miguel <bjornmigueloficial@icloud.com>
2022-03-23 23:59:40 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,28 @@
 <header class="header">
 
     <div class="flex">
-        
+        <a href="inicio.php" class="logo">iFood.</a>
+
+        <nav class="navbar">
+            <ul>
+                <li><a href="inicio.php">Início</a></li>
+                <li><a href="#">Páginas +</a>
+                        <ul>
+                            <li><a href="sobre.php">Sobre</a></li>
+                            <li><a href="contacto.php">Contacto</a></li>
+                        </ul>
+                </li>
+                <li><a href="comprar.php">Comprar</a></li>
+                <li><a href="ordens.php">Ordens</a></li>
+                <li><a href="conta.php">Conta +</a>
+                        <ul>
+                            <li><a href="entrar.php">Entrar</a></li>
+                            <li><a href="cadastrar.php">Cadastrar</a></li>
+                            
+                        </ul>
+                </li>
+            </ul>
+        </nav>
     </div>
 
 </header>
