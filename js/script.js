6512675fa2d7309e195b076f453d8af865ed6b15
Bjorn Miguel <bjornmigueloficial@icloud.com>
2022-03-25 22:40:28 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
+// Entrar/ Cadastrar
 let cadastrarBtn = document.querySelector('.conta-form .cadastrar-btn');
 let entrarBtn = document.querySelector('.conta-form .entrar-btn');
 
 cadastrarBtn.onclick = () =>{
     cadastrarBtn.classList.add('ativo');
     entrarBtn.classList.remove('ativo');
-}
+    document.querySelector('.conta-form .entrar-form').classList.remove('ativo');
+    document.querySelector('.conta-form .cadastrar-form').classList.add('ativo');
+};
+entrarBtn.onclick = () =>{
+    cadastrarBtn.classList.remove('ativo');
+    entrarBtn.classList.add('ativo');
+    document.querySelector('.conta-form .entrar-form').classList.add('ativo');
+    document.querySelector('.conta-form .cadastrar-form').classList.remove('ativo');
+};
+
+
+let contaForm = document.querySelector('.conta-form');
+document.querySelector('#acesso-btn').onclick = () =>{
+    contaForm.classList.add('ativo');
+};
+document.querySelector('#fechar-form').onclick = () =>{
+    contaForm.classList.remove('ativo');
+};
+
