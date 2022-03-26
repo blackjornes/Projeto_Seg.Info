5ade067bb1ed5fe0c30a3adcd1e3c42a3c557567
Bjorn Miguel <bjornmigueloficial@icloud.com>
2022-03-26 12:10:28 +0100

    Update footer.php

---
+++ php/footer.php
@@ -0,0 +1,13 @@
+<section class="footer">
+    <div class="box-container">
+        
+        <div class="box">
+            <h3>Links rápidos</h3>
+            <a href="../inicio.php">Início</a>
+            <a href="../sobre.php">Sobre</a>
+            <a href="../contacto.php">Contacto</a>
+            <a href="../comprar.php">Comprar</a>
+        </div>
+
+    </div>
+</section>
