53d266848d0a6bde276df85031fe58717b5014d9
Bjorn Miguel <bjornmigueloficial@icloud.com>
2022-03-26 17:46:56 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ window.onscroll = () =>{
     navbar.classList.remove('ativo');
 }
 
-
 // Entrar/ Cadastrar
 let cadastrarBtn = document.querySelector('.conta-form .cadastrar-btn');
 let entrarBtn = document.querySelector('.conta-form .entrar-btn');
@@ -28,7 +27,6 @@ entrarBtn.onclick = () =>{
     document.querySelector('.conta-form .entrar-form').classList.add('ativo');
     document.querySelector('.conta-form .cadastrar-form').classList.remove('ativo');
 };
-
 
 let contaForm = document.querySelector('.conta-form');
 document.querySelector('#acesso-btn').onclick = () =>{
