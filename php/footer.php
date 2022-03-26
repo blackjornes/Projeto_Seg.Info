15044e6faace96ca1402b656a8ad0f67446eab35
Bjorn Miguel <bjornmigueloficial@icloud.com>
2022-03-26 14:30:11 +0100

    Update footer.php

--- php/footer.php
+++ php/footer.php
@@ -9,5 +9,22 @@
             <a href="../comprar.php">Comprar</a>
         </div>
 
+        <div class="box">
+            <h3>Informação de contacto</h3>
+            <p><i class="fas fa-phone"></i>(+244) 947 - 714 - 575</p>
+            <p><i class="fas fa-phone"></i>(+244) 997 - 714 - 575</p>
+            <p><i class="fas fa-map-marker-alt"></i>Lubango, Cristo Rei</p>
+        </div>
+
+        <div class="box">
+            <h3>Redes sociais</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+            <a href="#"><i class="fab fa-youtube"></i>YouTube</a>
+        </div>
+
     </div>
+
+    <div class="creditos">&copy; Direitos autorais @ <?php echo date('Y'); ?> | <span>Grupo 6</span> </div>
 </section>
